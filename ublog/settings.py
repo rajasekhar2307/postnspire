3bd1c7c0d5f9683f45ed27ea69ea6e7d9e46dd13
Raja Sekhar <rajasekhar.b237@gmail.com>
2021-06-30 14:35:51 +0530

    Changed the email details

--- ublog/settings.py
+++ ublog/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_mail_here'
-EMAIL_HOST_PASSWORD = 'your_mail_password_here'
+EMAIL_HOST_USER = 'help.PostnSpire@gmail.com'
+EMAIL_HOST_PASSWORD = 'Post@1024'
